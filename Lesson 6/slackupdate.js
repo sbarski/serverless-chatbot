7d556897486f3fd53795907b7e33252e5cc6b3a3
John McKim <johncmckim@gmail.com>
2017-04-25 11:47:01 -0500

    Add more logging and update bucket names

--- Lesson 6/slackupdate.js
+++ Lesson 6/slackupdate.js
@@ -6,26 +6,36 @@ const qs = require('querystring');
 const request = require('request');
 const s3 = new aws.S3();
 
-const getSignedUrl = function(bucket, key) {
+const getSignedUrl = function (bucket, key) {
+    console.log('Getting signed url for bucket');
+
     return new Promise((resolve, reject) => {
-        const params = {Bucket: bucket, Key: key, Expires: 604800};
+        const params = {
+            Bucket: bucket,
+            Key: key,
+            Expires: 604800
+        };
         const url = s3.getSignedUrl('getObject', params);
         console.log(url);
         resolve(url);
     });
 };
 
-const getShortUrl = function(url) {
+const getShortUrl = function (url) {
+    console.log('Getting short url');
+
     return new Promise((resolve, reject) => {
         const req = {
-            uri: process.env.SHORTENER_API_URL + qs.stringify({key: process.env.SHORTENER_API_KEY}),
-            method: 'POST', 
+            uri: process.env.SHORTENER_API_URL + qs.stringify({
+                key: process.env.SHORTENER_API_KEY
+            }),
+            method: 'POST',
             json: true,
             body: {
                 longUrl: url
             }
         }
-        
+
         request(req, (err, res, body) => {
             if (err && res.statusCode !== 200) {
                 reject(err);
@@ -36,30 +46,38 @@ const getShortUrl = function(url) {
     });
 }
 
-const writeToSlack = function(url) {
-  return new Promise((resolve, reject) => {
-    const slackParams = {
-        token: process.env.BOT_ACCESS_TOKEN,
-        channel: process.env.CHANNEL_ID,
-        text: url
-      }
+const writeToSlack = function (url) {
+    console.log('Posting image back to slack');
 
-      const slackurl = process.env.POST_MESSAGE_URL + qs.stringify(slackParams);
+    return new Promise((resolve, reject) => {
+        const slackParams = {
+            token: process.env.BOT_ACCESS_TOKEN,
+            channel: process.env.CHANNEL_ID,
+            text: url
+        }
 
-      https.get(slackurl, (res) => {
-        const statusCode = res.statusCode;
-        resolve();
-      }) 
-  });
+        const slackurl = process.env.POST_MESSAGE_URL + qs.stringify(slackParams);
+
+        https.get(slackurl, (res) => {
+            const statusCode = res.statusCode;
+            resolve();
+        })
+    });
 }
 
-module.exports.execute = (event, context, callback) => {  
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+module.exports.execute = (event, context, callback) => {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
 
-  getSignedUrl(bucket, key)
-    .then((url) => getShortUrl(url))
-    .then((url) => writeToSlack(url))
-    .then(() => callback(null))
-    .catch((err) => callback(err))
+    getSignedUrl(bucket, key)
+        .then((url) => getShortUrl(url))
+        .then((url) => writeToSlack(url))
+        .then(() => {
+            console.log('Finished processing image');
+            callback(null);
+        })
+        .catch((err) => {
+            console.log(err);
+            callback(err);
+        });
 };
